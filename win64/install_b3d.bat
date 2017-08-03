db551a760d470eaf6e02299c6bb19a32e3d8baa1
Balint Balazs - 515 <balint.balazs@embl.de>
2017-08-03 17:26:33 +0200

    updates install_b3d.bat to include license prompt

--- win64/install_b3d.bat
+++ win64/install_b3d.bat
@@ -1,5 +1,16 @@
 @echo off
 
+type LICENSE
+echo(
+:ask
+echo Please take a moment to read the above license agreement now. Do you
+echo agree, and wish to proceed with the installation? (Y/N)
+set INPUT=
+set /P INPUT=%=%
+If /I "%INPUT%"=="y" goto yes 
+If /I "%INPUT%"=="n" goto no
+goto ask
+:yes
 set folder=%~dp0
 IF %folder:~-1%==\ SET folder=%folder:~0,-1%
 rem echo %folder%
@@ -9,11 +20,21 @@ if defined HDF5_PLUGIN_PATH (
 	if "%folder%"=="%HDF5_PLUGIN_PATH%" (
 		echo Everything is already set up.
 	) else (
-		echo Copying dlls to %HDF5_PLUGIN_PATH%
-		copy /-Y "%folder%\*.dll" "%HDF5_PLUGIN_PATH%"
+		if exist "%HDF5_PLUGIN_PATH%" (
+			echo Copying dlls to %HDF5_PLUGIN_PATH%
+			copy /-Y "%folder%\*.dll" "%HDF5_PLUGIN_PATH%"
+		) else (
+			echo HDF5_PLUGIN_PATH is defined, but location is not found. Setting it to current directory.
+			setx HDF5_PLUGIN_PATH "%folder%"
+		)
 	)
 ) else (
 	echo HDF5_PLUGIN_PATH not defined. Setting it to current directory.
 	setx HDF5_PLUGIN_PATH "%folder%"
 )
+echo B3D is successfully installed.
+goto end
+:no
+echo Quitting installation.
+:end
 pause
